a4574e74b0a115f1493f50dfba23b2e9ce3f5b01
Jesus Akanle <jesuswrites20043@gmail.com>
2023-07-17 09:49:31 +0000

    Started Models

---
+++ Models/Users.ts
@@ -0,0 +1,18 @@
+
+class Users {
+    private firstName: string;
+    private lastName: string;
+    private email: string;
+    private gender: string;
+
+    constructor(firstName, lastName, email, gender){
+        this.email = email;
+        this.gender = gender;
+        this.lastName = lastName;
+        this.firstName = firstName;
+    }
+
+    get fullName() {
+        return this.firstName
+    }
+}
