2579f1c0a326b7a0b6958f4591e24ca0ad29dae7
JesusOfLagos <jesuswrites20043@gmail.com>
2023-08-02 01:38:11 +0100

    Initialized first test

---
+++ Tests/users.model.test.ts
@@ -0,0 +1,19 @@
+import Users from './Users';
+
+describe('Users class', () => {
+  // Test the constructor
+  test('should create a new user object', () => {
+    const user = new Users('John', 'Doe', 'john@example.com', 'male');
+    expect(user).toBeDefined();
+    expect(user.firstName).toBe('John');
+    expect(user.lastName).toBe('Doe');
+    expect(user.email).toBe('john@example.com');
+    expect(user.gender).toBe('male');
+  });
+
+  // Test the fullName getter
+  test('should return the full name of the user', () => {
+    const user = new Users('John', 'Doe', 'john@example.com', 'male');
+    expect(user.fullName).toBe('John Doe');
+  });
+});
