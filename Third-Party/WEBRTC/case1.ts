eef934d79dbedbbd25d726f9ecf3239eed78c3ac
JesusOfLagos <jesuswrites20043@gmail.com>
2023-08-02 12:56:43 +0100

    added uninteresting stuffs

--- Third-Party/WEBRTC/case1.ts
+++ Third-Party/WEBRTC/case1.ts
@@ -44,3 +44,52 @@ async function init() {
 }
 
 init();
+
+
+// public/index.ts
+let localStream: MediaStream;
+const localVideo = document.getElementById("localVideo") as HTMLVideoElement;
+const remoteVideo = document.getElementById("remoteVideo") as HTMLVideoElement;
+
+const configuration: RTCConfiguration = {
+  iceServers: [
+    {
+      urls: "stun:stun.l.google.com:19302",
+    },
+  ],
+};
+
+let peerConnection: RTCPeerConnection;
+
+async function createPeerConnection(): Promise<RTCPeerConnection> {
+  peerConnection = new RTCPeerConnection(configuration);
+
+  localStream.getTracks().forEach((track) => peerConnection.addTrack(track, localStream));
+
+  peerConnection.onicecandidate = (event) => {
+    if (event.candidate) {
+      // Send the ICE candidate to the remote peer using your P2P signaling method
+      // (implement this part in your backend or P2P signaling library)
+    }
+  };
+
+  peerConnection.ontrack = (event) => {
+    remoteVideo.srcObject = event.streams[0];
+  };
+
+  // Implement other event listeners for connection state, errors, etc. if needed
+
+  return peerConnection;
+}
+
+async function init(): Promise<void> {
+  try {
+    localStream = await navigator.mediaDevices.getUserMedia({ video: true, audio: true });
+    localVideo.srcObject = localStream;
+  } catch (error) {
+    console.error("Error accessing media devices:", error);
+  }
+}
+
+init();
+
--- Third-Party/WEBRTC/case3.ts
+++ Third-Party/WEBRTC/case3.ts
@@ -1,10 +1,10 @@
-// server.js
-const WebSocket = require("ws");
+// server.ts
+import WebSocket from "ws";
 
 const wss = new WebSocket.Server({ port: 3000 });
 
-wss.on("connection", (ws) => {
-  ws.on("message", (message) => {
+wss.on("connection", (ws: WebSocket) => {
+  ws.on("message", (message: WebSocket.Data) => {
     // Forward the signaling message to the other peer(s)
     wss.clients.forEach((client) => {
       if (client !== ws && client.readyState === WebSocket.OPEN) {
