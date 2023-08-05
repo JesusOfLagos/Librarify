04d763dcbeec21be429ca699f60cd39f710d4313
JesusOfLagos <jesuswrites20043@gmail.com>
2023-08-05 13:46:13 +0100

    fixed library controller

--- Controllers/Services/libraryController.ts
+++ Controllers/Services/libraryController.ts
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 class CustomNode {
     public readonly value: number;
     public next: CustomNode | null = null;
@@ -27,51 +27,13 @@ class LinkedList {
 
         currentNode.next = newNode
     }
-}
 
 
-
-function main() {
-    const list = new LinkedList();
-
-    list.add(10);
-    list.add(20);
-    list.add(30);
-    list.add(40);
-
-    console.log(list)
-}
-
-
-main()
-=======
-class CustomNode {
-    public readonly value: number;
-    public next: CustomNode | null = null;
-
-    constructor(value: number) {
-        this.value = value;
-    }
-}
-
-class LinkedList {
-    private head: CustomNode | null = null;
-
-    add(value: number): void {
+    addNewHead(value: number): void {
         const newNode = new CustomNode(value);
 
-
-        if(this.head === null) {
-            this.head = newNode;
-            return;
-        }
-
-        let currentNode = this.head;
-        while(currentNode.next != null){
-            currentNode = currentNode.next
-        }
-
-        currentNode.next = newNode
+        newNode.next = this.head
+        this.head = newNode
     }
 }
 
@@ -84,10 +46,10 @@ function main() {
     list.add(20);
     list.add(30);
     list.add(40);
+    list.addNewHead(70)
 
     console.log(list)
 }
 
 
 main()
->>>>>>> e569dcb8eeee1f5aabdc662d502082a2ff42ac55
