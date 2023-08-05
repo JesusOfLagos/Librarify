7e8e2dbf6ea5122ff78c15282b57220bcff0e6f9
JesusOfLagos <jesuswrites20043@gmail.com>
2023-08-05 12:45:56 +0100

    I fixed bugssssss at app.ts

--- app.ts
+++ app.ts
@@ -1,22 +1,97 @@
 
-import express from 'express';
-import bodyParser from 'body-parser';
 
-const app = express();
+// Import modules
+import session from 'express-session';
+import express, { Application, Request, Response } from 'express';
+import { connect } from 'mongoose';
+import morgan from 'morgan';
+import cors from 'cors';
+import path from 'path';
+// import bodyParser from 'body-parser';
+// const session = require('express-session');
+// const cors = require('cors');
+const bodyParser = require('body-parser');
+import passport from 'passport';
+import socketio, { Server as SocketIOServer, Socket } from 'socket.io';
 
+import env from 'dotenv';
+import http from 'http';
+import dotenv from 'dotenv';
+dotenv.config();
+
+
+// ... Rest of the code ...
+;
+
+// Import routes
+// import UserRoutes from './Routes/users';
+// import MessagesRoutes from './Routes/messages';
+
+// Create express app
+const app: Application = express();
+const server: http.Server = http.createServer(app);
+// const io: SocketIOServer = socketio(server);
+const io: SocketIOServer = new SocketIOServer(server);
+
+
+// Middleware
+app.use(express.json());
+// app.use(morgan('dev'));
+app.use(cors({ origin: 'https://localhost:5000', credentials: true }));
+app.use(
+  session({
+    secret: 'my-secret',
+    resave: false,
+    saveUninitialized: true,
+  })
+);
 app.use(bodyParser.json());
-app.use(express.json())
+app.use(bodyParser.urlencoded({ extended: true }));
 
-const UserRoutes = require("./Routes/Auth/user")
-app.use('/users', UserRoutes)
+// Use routes
+// app.use('/', UserRoutes);
+// app.use('/', MessagesRoutes);
 
-
-
-
- 
- 
+// Connect to the database
+// Import dotenv config
 
 
+// ... Other imports and middleware ...
 
+// Connect to the database
+async function connectToDB() {
+    try {
+      const mongoURI = process.env.MONGO_URI ?? '';
+      await connect(mongoURI, {});
+      console.log('DB Connection was successful');
+    } catch (err) {
+      console.error('Oops!, an error occurred!', err);
+    }
+  }
+  
+
+// Call the connectToDB function
+connectToDB();
+
+
+// Port
+const port: number = Number(process.env.PORT) || 8090;
+
+
+
+// Socket.IO connection handling
+io.on('connection', (socket: Socket) => {
+    console.log('A user connected');
+  
+    // ... (rest of your socket.io code)
+  });
+  
+  // Listener
+  server.listen(port, () => {
+    // if (err) {
+    //   console.log(err);
+    // }
+    console.log(`Server Is Running on Port ${port}`);
+  });
 
 
